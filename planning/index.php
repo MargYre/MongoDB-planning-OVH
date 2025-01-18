75d7e366c5ac3919673615017e654245ac84ad68
margaux <margaux.tarayre@gmail.com>
2025-01-18 23:53:33 +0100

    Reset repo, keeping only README

--- planning/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';  // Pour charger les dépendances
-require 'config/database.php';
-require 'controllers/PlanningController.php';
-
-$database = new Database();
-$db = $database->getConnection();
-$controller = new PlanningController($db);
-$controller->index();
